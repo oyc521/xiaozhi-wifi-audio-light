bd45253dfb6e3a1dc8ebd0121bf575aca6162eb8
oyc521 <oyc18297657530@qq.com>
2026-09-25 14:43:16 +0800

    chore: remove tracked downloaded launcher

--- components/eda_web_console/start_esp_audio.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-curl -s "http://10.35.24.126/loopback.py" -o "%TEMP%\esp_loopback.py"
-echo Installing Python deps (first time needs internet)...
-python -m pip install pyaudiowpatch numpy -q
-echo Starting stream. Close this window to stop.
-python "%TEMP%\esp_loopback.py" 10.35.24.126 5004
-if errorlevel 1 ( echo Failed. Make sure Python is installed and on PATH. )
-pause
